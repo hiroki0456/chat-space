90254ba33b3e0cf081ec59b71594f86cd64461ad
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2020-02-10 11:11:56 +0900

    Ajax_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,36 +1,6 @@
 $(function(){
-  function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
-    if (message.image) {
-      var html = 
-      `<div class="chat-main__message-list__message-box">
-        <div class="chat-main__message-list__message-box__name-data>
-          ${message.user_name}
-          ${message.created_at}
-        </div>
-        <div class="chat-main__message-list__message-box__message-data">
-          ${message.content}
-          <img class="lower-message__image"src=${message.image} >
-        </div>
-      </div>`
-        return html;
-        //メッセージに画像が含まれない場合のHTMLを作る
-      } else {
-        var html =
-        `<div class="chat-main__message-list__message-box">
-            <chat-main__message-list__message-box__name-data">
-                ${message.user_name}
-                ${message.created_at}
-            </div>
-              <div class="chat-main__message-list__message-box__message-data"">
-                ${message.content}
-            </div>
-        </div>`
-        return html;
-      };
-    }
   $('#new_message').on('submit', function(e){
-    e.preventDefault();
+    e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -43,8 +13,47 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html);      
-      $('form')[0].reset();
+      $('.chat-main__message-list').append(html);      
+       $('form')[0].reset();
+       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+       $('input[name="commit"]').prop('disabled', false);
     })
+    
+    .always(function(){
+      $('input[name="commit"]').prop('disabled', false);
+    })
+
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
   });
-});
+  });
+  });
+
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = 
+    `<div class = "chat-main__message-list__message-box">
+      <div class ="chat-main__message-list__message-box__name-data">
+         ${message.user_name}
+         ${message.created_at}
+        </div>
+       <div class="chat-main__message-list__message-box__message-data">
+         ${message.content}
+         <img class="lower-message__image" src=${message.image}>
+       </div>
+      </div>`
+    } else {
+      var html =
+    `<div class = "chat-main__message-list__message-box">
+      <div class="chat-main__message-list__message-box__name-data">
+         ${message.user_name}
+         ${message.created_at}
+        </div>
+       <div class="chat-main__message-list__message-box__message-data">
+         ${message.content}
+       </div>
+      </div>`
+    }
+    return html
+  }
