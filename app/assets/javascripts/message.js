42087e3db1741cd870b54fde3b63b107c74499df
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2020-02-11 10:55:50 +0900

    Ajax 更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,48 +12,47 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-main__message-list').append(html);      
-       $('form')[0].reset();
-       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-       $('input[name="commit"]').prop('disabled', false);
+    var html = buildHTML(data);
+    $('.chat-main__message-list').append(html);      
+    $('form')[0].reset();
+    $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    $('input[name="commit"]').prop('disabled', false);
     })
     
     .always(function(){
-      $('input[name="commit"]').prop('disabled', false);
+    $('chat-main__message-form__message-box__message-send').prop('disabled', false);
     })
 
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+    alert("メッセージ送信に失敗しました");
   });
   });
   });
 
   function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
     if (message.image) {
       var html = 
     `<div class = "chat-main__message-list__message-box">
-      <div class ="chat-main__message-list__message-box__name-data">
+       <div class ="chat-main__message-list__message-box__name-data">
          ${message.user_name}
          ${message.created_at}
-        </div>
+       </div>
        <div class="chat-main__message-list__message-box__message-data">
          ${message.content}
          <img class="lower-message__image" src=${message.image}>
        </div>
-      </div>`
+     </div>`
     } else {
       var html =
     `<div class = "chat-main__message-list__message-box">
-      <div class="chat-main__message-list__message-box__name-data">
+       <div class="chat-main__message-list__message-box__name-data">
          ${message.user_name}
          ${message.created_at}
-        </div>
+       </div>
        <div class="chat-main__message-list__message-box__message-data">
          ${message.content}
        </div>
-      </div>`
+     </div>`
     }
     return html
   }
